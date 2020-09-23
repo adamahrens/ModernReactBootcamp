51e85e77c1151abf1d35b872f3acd40dec8632c9
Adam Ahrens <adam.aahrens@gmail.com>
2020-09-22 20:48:02 -0500

    Adding another Greeting functional component

--- TasteOfReact/First_Component/index.js
+++ TasteOfReact/First_Component/index.js
@@ -8,17 +8,24 @@ class Hello extends React.Component {
 				<Pill name="CSS" />
 				<Pill name="HTML" />
 				<Pill name="REACT" />
+				<Greeting name="Adam" />
 			</div>
 		);
 	}
 }
 
+// Class Based Component
 class Pill extends React.Component {
 	render() {
 		return (
 			<div className='pill'>{this.props.name}</div>
 		)
 	}
+}
+
+// Functional Based Component
+function Greeting(props) {
+	return <h2>Good Day, {props.name}</h2>;
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
