881ebd8419dd51769253c9ad969af9441d872630
Adam Ahrens <adam.aahrens@gmail.com>
2020-11-15 20:58:05 -0600

    Tweaking the CSS for the dice when rolling

--- Dice/dice/src/Dice.js
+++ Dice/dice/src/Dice.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 class Dice extends Component {
+    constructor(props) {
+        super(props)
+
+        this.diceName = this.diceName.bind(this)
+    }
+
+    diceName() {
+        switch (this.props.number) {
+            case "1": return "fa-dice-one"
+            case "2": return "fa-dice-two"
+            case "3": return "fa-dice-three"
+            case "4": return "fa-dice-four"
+            case "5": return "fa-dice-five"
+            case "6": return "fa-dice-six"
+        }
+    }
+
     render() {
+        let correctDiceName = this.diceName()
+        let icon = `fas ${correctDiceName} fa-10x`
+        let containerClass = this.props.rolling ? "Dice wiggling" : "Dice"
         return (
-            <div>
-                <h1>Dice!</h1>
-            </div>
+            <div className={containerClass} >
+                <i className={icon}></i>
+            </div >
         )
     }
 }
--- Dice/dice/src/RollDice.js
+++ Dice/dice/src/RollDice.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react'
 import Dice from './Dice'
 
 class RollDice extends Component {
+
+    static defaultProps = {
+        dice: ['1', '2', '3', '4', '5', '6']
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = { dice1: '2', dice2: '4', isRolling: false };
+        this.rollDice = this.rollDice.bind(this)
+    }
+
+    rollDice() {
+        let die1 = Math.floor(Math.random() * this.props.dice.length)
+        let die2 = Math.floor(Math.random() * this.props.dice.length)
+        console.log(`Rolling ${die1} ${die2}`)
+        this.setState({ dice1: this.props.dice[die1], dice2: this.props.dice[die2], isRolling: true })
+
+        setTimeout(() => {
+            this.setState({ isRolling: false })
+        }, 1000);
+    }
+
     render() {
+        let buttonText = this.state.isRolling ? 'Rolling' : 'Roll Dice'
         return (
             <div>
                 <h1>Roll Some Dice!</h1>
-                <Dice number="1" />
-                <Dice number="2" />
-                <button>Roll Dice</button>
+                <div className="dice-container">
+                    <Dice number={this.state.dice1} rolling={this.state.isRolling} />
+                    <Dice number={this.state.dice2} rolling={this.state.isRolling} />
+                </div>
+                <button disabled={this.state.isRolling} onClick={this.rollDice}>{buttonText}</button>
             </div>
         )
     }
