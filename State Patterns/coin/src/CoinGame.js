200719b9293122d25dbadd39fdbc03a404995147
Adam Ahrens <adam.aahrens@gmail.com>
2020-11-16 13:04:34 -0600

    Adding a viewState method

--- State Patterns/coin/src/CoinGame.js
+++ State Patterns/coin/src/CoinGame.js
@@ -13,6 +13,7 @@ class CoinGame extends Component {
         this.state = { tosses: [] }
 
         this.flipCoin = this.flipCoin.bind(this)
+        this.reduceState = this.reduceState.bind(this)
     }
 
     flipCoin() {
@@ -22,21 +23,24 @@ class CoinGame extends Component {
         this.setState({ tosses: newTosses })
     }
 
-    render() {
+    reduceState() {
         let flips = this.state.tosses.length
         let heads = this.state.tosses.filter(c => c === HEADS).length
         let tails = this.state.tosses.filter(c => c === TAILS).length
         let imageSrc = this.state.tosses[this.state.tosses.length - 1] === HEADS ? HEADS_SRC : TAILS_SRC
-        console.log(imageSrc)
+        return { flips: flips, heads: heads, tails: tails, imageSrc: imageSrc }
+    }
 
+    render() {
+        let viewState = this.reduceState()
         return (
             <div className="CoinGame">
                 <h1>{this.props.header}</h1>
-                { flips > 0 &&
-                    <Coin imageSrc={imageSrc} />
+                { viewState.flips > 0 &&
+                    <Coin imageSrc={viewState.imageSrc} />
                 }
                 <button className="Coin-button" onClick={this.flipCoin}>Flip</button>
-                <p>Out of <strong>{flips}</strong> flips, there have been <strong>{heads}</strong> heads and <strong>{tails}</strong> tails</p>
+                <p>Out of <strong>{viewState.flips}</strong> flips, there have been <strong>{viewState.heads}</strong> heads and <strong>{viewState.tails}</strong> tails</p>
             </div >
         )
     }
