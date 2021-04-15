88c225b4d0ebc1f7aafd90bbbe4781f30682a270
Adam Ahrens <adam.aahrens@gmail.com>
2021-04-14 21:56:01 -0500

    Styling the box component, adding copy clipboard

--- Color/reactcolor/src/ColorBox.js
+++ Color/reactcolor/src/ColorBox.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './ColorBox.css'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
     render() {
+        const { name, background } = this.props
         return (
-            <div style={{ background: this.props.background }} className="ColorBox">
-                <span>{this.props.name}</span>
-                <span>MORE</span>
-            </div>
+            <CopyToClipboard text={background}>
+                <div style={{ background }} className="ColorBox">
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">
+                            Copy
+                        </button>
+                    </div>
+                    <span className="see-more">MORE</span>
+                </div>
+            </CopyToClipboard>
         )
     }
 }
