6ad3e73cce31d455dea978fbbe4951b7548ae2de
Adam Ahrens <adam.aahrens@gmail.com>
2021-04-26 21:00:32 -0500

    Adding overlay copy message

--- Color/reactcolor/src/ColorBox.js
+++ Color/reactcolor/src/ColorBox.js
@@ -3,11 +3,32 @@ import './ColorBox.css'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { copied: false }
+        this.changeCopyState = this.changeCopyState.bind(this)
+    }
+
+    changeCopyState() {
+        this.setState({ copied: true }, () => {
+            setTimeout(() => this.setState({ copied: false }), 1500)
+        })
+    }
     render() {
         const { name, background } = this.props
+        let overlayClassName = this.state.copied ? "copy-overlay show" : "copy-overlay"
+        let overlayMessageClassName = this.state.copied ? "copy-message show" : "copy-message"
+
         return (
-            <CopyToClipboard text={background}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className="ColorBox">
+                    <div style={{ background }} className={overlayClassName}>
+                    </div>
+                    <div className={overlayMessageClassName}>
+                        <h1>COPIED!</h1>
+                        <p>{background}</p>
+                    </div>
                     <div className="copy-container">
                         <div className="box-content">
                             <span>{name}</span>
